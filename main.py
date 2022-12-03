5c6830324c7b81414019baba8bcf50736d235115
tanmayvaij <tanmayvaij22@gmail.com>
2022-12-03 14:35:08 +0530

    added getuserfollower code

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ from instagrapi import Client
 
 client = Client()
 
+# Taking username and password of the user
+# username = input("Enter your username:- ")
+# password = input("Enter your password:- ")
+
 try:
 
-    pass
+    # Login into instagram account
+    client.login('tony_bot_224', 'tejomay1234')
+
+    # Get all following users
+    # user_following_list = list(map(int, client.user_following(client.user_id).keys()))
+
+    print(client.user_stories(8015684279))
 
 except Exception as e:
+
     print(f"Failed: {e}")

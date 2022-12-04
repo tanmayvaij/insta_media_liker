8afe186d3a3eef32200a703cf27e412ff2337d90
tanmayvaij <tanmayvaij22@gmail.com>
2022-12-04 10:46:47 +0530

    script scope changed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 '''
 
 
-    => Script for liking all the instgram stories of the people in your following list <=
+    => Script for liking all the instgram stories of follower of any user <=
 
     WARNING:- INSTAGRAM MIGHT BLOCK THE SCRIPT IF IT IS USED CONTINUOUSLY. YOU MIGHT
     SOMETIMES SEE A MESSAGE IN THE CONSOLE WHICH TELLS TO ENTER A 6 DIGIT NUMBER WHICH
@@ -15,7 +15,10 @@ from instagrapi import Client
 
 class Insta_Story_Liker:
 
-    def __init__(self, username: str, password: str):
+    def __init__(self, username: str, password: str, target: str):
+
+        # Set target profile
+        self.target = target
 
         # Created instance of instgrapi client
         self.client = Client()
@@ -27,15 +30,20 @@ class Insta_Story_Liker:
 
         print("---> Logged in successfully")
 
-    # Get all following users
-    def following_list(self):
+    # Get all follower users
+    def follower_list(self):
 
-        return list(map(int, self.client.user_following(self.client.user_id).keys()))
+        # Grabbing user id of target user
+        target_user_id = self.client.user_id_from_username(self.target)
+
+        # return list of follower's user id
+        return list(map(int, self.client.user_followers(target_user_id).keys()))
+
 
     # Get list of story ids
     def story_ids(self, users_list: list):
 
-        # All story pk emppty list initialized
+        # All story pk empty list initialized
         all_ids = []
 
         for user_id in users_list:
@@ -47,12 +55,13 @@ class Insta_Story_Liker:
             all_ids += single_user_ids
 
         # Logging all the fetched story ids
-        print("Got stories with the follwowing ids:- ")
+        print(f"Got {len(all_ids)} stories with the follwowing ids:- ")
 
         for id in all_ids:
             print(f"---> {id}")
 
         return all_ids
+
 
     # Method for liking stories
     def like_stories(self, id_list: list):
@@ -72,16 +81,19 @@ class Insta_Story_Liker:
                 print(f"---> Failed liking story with id -> {i}")      
 
 
-if __name__ == '__main__':
+
+def main():
 
     username = input("Enter your username:- ")
 
     password = input("Enter your password:- ")
 
+    target = input("Enter target username:- ")
+
     try:
     
         # Initialized Insta_Story_Liker instance
-        liker = Insta_Story_Liker(username, password)
+        liker = Insta_Story_Liker(username, password, target)
 
         # Got all the following users list
         users_list = liker.following_list()
@@ -97,3 +109,9 @@ if __name__ == '__main__':
     except Exception as e:
 
         print(f"Failed: {e}")
+
+
+if __name__ == '__main__':
+
+    main()
+    
